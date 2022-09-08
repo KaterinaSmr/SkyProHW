5c02c12ff46378d9f71707135cdac7ff300e55d9
Ekaterina Gorbacheva <katerina.smr@gmail.com>
2022-09-08 14:35:27 +0300

    Hw for Cycles lesson

---
+++ hw4_cycles/src/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+    }
+    public static void task1(){
+        int i = 0;
+        while (i < 10){
+            i++;
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+        for (int j = 10; j > 0 ; j--) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+    }
+
+    public static void task2(){
+        int firstFridayOfAMonth = 3;
+        final int DAYS_IN_MONTH = 31;
+
+        for (int i = 1; i <= DAYS_IN_MONTH; i++) {
+            if (i % 7 == firstFridayOfAMonth)
+                System.out.println("Сегодня пятница, " + i + "-е число. Необходимо подготовить отчет.");
+        }
+    }
+
+    public  static void task3(){
+        int currentYear = 2022;
+        int startYear = currentYear - 200;
+        final int PERIOD = 79;
+        int i = startYear;
+
+        while (i < (currentYear + PERIOD)){
+            if (i % PERIOD == 0)
+                System.out.println(i);
+            i++;
+        }
+    }
+}
