95b06ea4c035e8a7e8910885609cef025c486cfe
Ekaterina Gorbacheva <katerina.smr@gmail.com>
2022-09-09 18:56:23 +0300

    hw8 for Methods lesson

---
+++ hw8_methods/src/Main.java
@@ -0,0 +1,48 @@
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        //task1
+        checkLeapYear(2021);
+
+        //task2
+        defineAppVersion(1, 2020);
+        defineAppVersion(0, 2022);
+
+        //task3
+        System.out.println("Потребуется дней " + calculateDeliveryDays(95));
+    }
+
+    private static void checkLeapYear(int year){
+        if (year % 400 == 0)
+            System.out.println(year + " - високосный год");
+        else if (year % 100 == 0)
+            System.out.println(year + " - не високосный год");
+        else if (year % 4 == 0)
+            System.out.println(year + " - високосный год");
+        else
+            System.out.println(year + " - не високосный год");
+    }
+
+    private static void defineAppVersion(int clientOS, int clientDeviceYear){
+        String os;
+        String version="";
+
+        if (clientOS==0)
+            os = "iOS";
+        else if (clientOS == 1)
+            os = "Android";
+        else {
+            System.out.println("Операционная система не может быть определена");
+            return;
+        }
+
+        if (clientDeviceYear < LocalDate.now().getYear())
+            version = "облегченную ";
+        System.out.println("Установите " + version + "версию приложения для " + os + " по ссылке");
+    }
+
+    private static int calculateDeliveryDays(int deliveryDistance){
+        return 1 + (deliveryDistance+20)/40 ;
+    }
+}
